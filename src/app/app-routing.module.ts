3f95bdc5852a2a64bc37424e8bdc18794f8f5446
gmc456 <gmc456@inlumine.ual.es>
2023-05-20 16:29:14 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { RoomComponent } from './components/room/room.component';
+import { BuildingComponent } from './components/building/building.component';
 
-const routes: Routes = [];
+//const routes: Routes = [];
+
+export const ROUTES: Routes = [
+  { path: 'home', component: HomeComponent }, 
+  { path: 'about', component: AboutComponent },
+  { path: 'building', component: BuildingComponent }, 
+  { path: 'room', component: RoomComponent }, 
+  { path: '', pathMatch: 'full', redirectTo: 'home' }, 
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }, 
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  //imports: [RouterModule.forRoot(routes)],
+  imports: [BrowserModule, RouterModule.forRoot(ROUTES, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { MatIconModule } from '@angular/material/icon';
+import { BuildingComponent } from './components/building/building.component';
+import { RoomComponent } from './components/room/room.component';
+import { MapComponent } from './components/map/map.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    NavbarComponent,
+    BuildingComponent,
+    RoomComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
